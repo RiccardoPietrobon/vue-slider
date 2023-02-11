164176d582fa948f6ef84ba92e21959e08bc45b5
RiccardoPietrobon <riccardopietrobon.sanso@gmail.com>
2023-02-11 08:25:40 +0100

    active - no active

--- JS/app.js
+++ JS/app.js
@@ -3,15 +3,24 @@ const app = Vue.createApp({
     data() {
         return {
             images: [
-                "./img/01.webp",
-                "./img/02.webp",
-                "./img/03.webp",
-                "./img/04.webp",
-                "./img/05.webp",
-            ]
+                "./img/01.webp", //indice 1
+                "./img/02.webp", //indice 2
+                "./img/03.webp", //indice 3
+                "./img/04.webp", //indice 4
+                "./img/05.webp", //indice 5
+            ],
+
+            activeImage: 0
         }
     }
 
 });
 
-app.mount('#root');
+app.mount('#app');
+
+
+
+
+//registrazione 32 min!!!!!
+
+
